314d5bd7d956fec78c67a90f6856ab33666b5ca2
unknown <hkaur@contriverz.com>
2021-06-04 15:30:01 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CapturePoiComponent } from './capture-poi/capture-poi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapturePoiComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
